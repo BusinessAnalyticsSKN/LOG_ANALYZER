2fcbb09ebcad6eaa1631c96e0005e47f7fbefca7
BusinessAnalyticsSKN <161829568+BusinessAnalyticsSKN@users.noreply.github.com>
2024-03-01 11:42:07 +0100

    Add files via upload

---
+++ batch_log_analyzer.bat
@@ -0,0 +1,5 @@
+c:
+cd c:\karol\skn_ba\nowe_SAS_Viya\log_analyzer\PROCSS_SIMULATION\codes\
+
+"C:\Program Files\SASHome\SASFoundation\9.4\sas.exe" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\en\sasv9.cfg" -rsasuser -rsasuser -NOSYNTAXCHECK -sysin log_analyzer.sas -log log_analyzer.log
+
